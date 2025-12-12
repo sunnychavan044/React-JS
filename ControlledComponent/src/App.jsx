2ce9fee15c0ef044537f933eab0757e36e27bd46
Sunny Chavan <sunnychavan0403@gmail.com>
2025-12-12 11:28:18 +0530

    push

--- ControlledComponent/src/App.jsx
+++ ControlledComponent/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Controlled Component</h1>
+      <form action="" method="get">
+        <input type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder="Enter Name" />
+        <br />
+        <br />
+        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter email" />
+        <br /> <br />
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Enter Password" />
+        <br /> <br />
+        <button type="submit">Submit</button>
 
-export default App
+        <button onClick={() =>{setName('');setEmail('');setPassword('');}}>Clear</button>
+        <h3>{name}</h3>
+        <h3>{email}</h3>
+        <h3>{password ? "*".repeat(password.length) : ""}</h3>
+      </form>
+    </div>
+  );
+}
+export default App;
